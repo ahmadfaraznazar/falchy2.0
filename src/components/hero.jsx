e18c1b48ef9ef634008d85b31c40e49ddd071693
Ahmed Nazar <110450057+ahmadfaraznazar@users.noreply.github.com>
2025-02-17 16:39:31 +0500

    add form to hero (WIP)

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,5 +1,6 @@
 import '../styles/hero.css'
 import HeroImage from '../assets/Hero-Car.png'
+import BookingIcon from '../assets/booking-icon.svg'
 
 const hero = () => {
   return (
@@ -26,13 +27,46 @@ const hero = () => {
             </div>
 
             <div className='car-image'>
-                <image src={HeroImage} alt='car image' width='1155px'></image>
+                <image src={HeroImage} alt='car image' className='hero-car-image'></image>
             </div>
             
 
         </div>
 
         <div className='hero-form'>
+            
+
+        <form>
+
+        <div className='input-container'>
+            <label htmlFor="pickup-address" className='hero-form-label'>Pickup Address:</label>
+            <input type="text" id="pickup-address" name="pickup-address" className='hero-form-input' placeholder='enter pickup address' required/>
+        </div>
+
+        <div className='input-container' >
+           <label htmlFor="dropoff-address" className='hero-form-label' >Dropoff Address:</label>
+           <input type="text" id="dropoff-address" className='hero-form-input' placeholder='enter dropoff address' name="dropoff-address" required/>
+        </div>
+
+        <div className='input-container'>
+            <label htmlFor="dropoff-date" className='hero-form-label'>Dropoff Date:</label>
+            <input type="date" id="dropoff-date" name="dropoff-date" className='hero-form-input' required/>
+        </div>
+
+        
+
+        <div className='input-container'>
+            <label htmlFor="pickup-date" className='hero-form-label'>Pickup Date:</label>
+            <input type="date" id="pickup-date" name="pickup-date" className='hero-form-input' required/>
+        </div>
+
+        
+
+        <button className='hero-form-btn' type="submit">
+            <img src={BookingIcon} alt='booking-icon' ></img>
+            <p>Book Now</p>
+        </button>
+        </form>
             
 
         </div>
